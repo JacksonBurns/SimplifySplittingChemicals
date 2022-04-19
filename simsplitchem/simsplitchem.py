7ebac13c24274ce134c250cb8d10f3f9089c79c3
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2022-04-19 11:18:45 -0400

    Update simsplitchem.py

--- simsplitchem/simsplitchem.py
+++ simsplitchem/simsplitchem.py
@@ -1,3 +1,13 @@
 
-def placeholder():
+def simsplitchem(
+    X,
+    y=None,
+    method='random',
+    hdict=None,
+    test_size=0.25,
+    train_size=0.75,
+    shuffle=False,
+):
     return
+    
+        
